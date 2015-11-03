d97f4963eb9b574e9cfbf17fd7320f509dcb8a90
maulin <maulin.c.desai@gmail.com>
2015-11-03 11:08:24 +0530

    store current changes

---
+++ app/src/main/java/com/example/maulin/myappportfolio/HomeScreenActivity.java
@@ -0,0 +1,44 @@
+package com.example.maulin.myappportfolio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickButton1(View view) {
+        Toast.makeText(this,R.string.media_streamer,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton2(View view) {
+        Toast.makeText(this,R.string.super_duo1,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton3(View view) {
+        Toast.makeText(this,R.string.super_duo2,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton4(View view) {
+        Toast.makeText(this,R.string.ant_terminator,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton5(View view) {
+        Toast.makeText(this,R.string.materialize,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton6(View view) {
+        Toast.makeText(this,R.string.capstone,Toast.LENGTH_SHORT).show();
+    }
+}
