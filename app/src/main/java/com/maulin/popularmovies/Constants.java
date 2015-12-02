885246a69ad6886bdf3a721ea3ff80e10e840862
maulin <maulin.c.desai@gmail.com>
2015-12-02 22:52:03 +0530

    remove sensitive info

---
+++ app/src/main/java/com/maulin/popularmovies/Constants.java
@@ -0,0 +1,20 @@
+package com.maulin.popularmovies;
+
+/**
+ * Created by maulin on 26/11/15.
+ */
+public class Constants {
+    /**
+     * image url of tmdb
+     */
+    public static String TMDB_IMAGE_URL="http://image.tmdb.org/t/p/";
+
+    /**
+     * API KEY for tmdb
+     */
+    public static String API_KEY="PLACE YOUR KEY HERE";
+
+    //TMDB discover movie
+    public static String TMDB_DISCOVER_MOVIE="http://api.themoviedb.org/3/discover/movie?api_key="+API_KEY;
+}
+
